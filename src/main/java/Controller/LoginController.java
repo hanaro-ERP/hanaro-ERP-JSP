115f6e088b6b7586f433b3c4832fd17b6ebe6613
lake041 <lake041@korea.ac.kr>
2023-06-17 05:14:00 +0900

    Fix: 로그인 URL

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -2,9 +2,7 @@ package Controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
--- src/main/java/DTO/EmployeeDTO.java
+++ src/main/java/DTO/EmployeeDTO.java
@@ -1,53 +1,16 @@
 package DTO;
 
+import lombok.*;
+
+@Getter
+@Setter
+
 public class EmployeeDTO {
-	private int employeeId, bankId;
-	private String password, eName, department, position, isAdmin;
+	private int employeeId, bankId, isAdmin;
+	private String password, eName, department, position;
 
 	public EmployeeDTO(int employeeId, String password) {
 		this.employeeId = employeeId;
 		this.password = password;
-	}	
-	public int getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-	public int getBankId() {
-		return bankId;
-	}
-	public void setBankId(int bankId) {
-		this.bankId = bankId;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String geteName() {
-		return eName;
-	}
-	public void seteName(String eName) {
-		this.eName = eName;
-	}
-	public String getDepartment() {
-		return department;
-	}
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-	public String getPosition() {
-		return position;
-	}
-	public void setPosition(String position) {
-		this.position = position;
-	}
-	public String getIsAdmin() {
-		return isAdmin;
-	}
-	public void setIsAdmin(String isAdmin) {
-		this.isAdmin = isAdmin;
 	}
 }
