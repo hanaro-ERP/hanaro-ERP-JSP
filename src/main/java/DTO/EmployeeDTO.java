489bd30aa569d3aed967a3148a7a15cdbb89762d
김민재 <lake041@korea.ac.kr>
2023-06-16 14:19:01 +0900

    Feat: 로그인 기초 개발 (#25)
    * Feat: 로그인 기초 개발
    
    * Update: 개행 수정
    
    * Update: 개행 수정
    
    * Style: login 스타일
    
    * Update: 입출금 내역 팝업 너비 수정
    
    * Update: 계좌 검색 직접 입력 수정
    
    * Feat: 로그인 기초 개발
    
    * Update: 코드 간소화

---
+++ src/main/java/DTO/EmployeeDTO.java
@@ -0,0 +1,53 @@
+package DTO;
+
+public class EmployeeDTO {
+	private int employeeId, bankId;
+	private String password, eName, department, position, isAdmin;
+
+	public EmployeeDTO(int employeeId, String password) {
+		this.employeeId = employeeId;
+		this.password = password;
+	}	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public int getBankId() {
+		return bankId;
+	}
+	public void setBankId(int bankId) {
+		this.bankId = bankId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String geteName() {
+		return eName;
+	}
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getIsAdmin() {
+		return isAdmin;
+	}
+	public void setIsAdmin(String isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+}
