c205ab2ba3a4907635086fa1aa7a70e196dd0d0e
ssangjun <ssangjun4dev@gmail.com>
2023-06-18 01:29:23 +0900

    Feat: 고객 목록 컨트롤러 작업

---
+++ src/main/java/Controller/CustomerListController.java
@@ -0,0 +1,45 @@
+package Controller;
+
+import java.io.*;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/customerList")
+public class customerListController extends HttpServlet {
+	public customerListController() {
+		super();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		postcustomerListProcess(request, response);
+	}
+	
+	protected void postcustomerListProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String[] infos = {"customerName", "jobCode", "customerEmployee", "bankLocation", "customerAge", "customerGender", "customerGrade", "customerCredit", "customerCountry", "disabilityGrade", "citySelect", "district"};
+			
+			for (int i = 0; i < infos.length; i++) {
+			    String[] selectedJobs = request.getParameterValues(infos[i]);
+			    if (selectedJobs != null) {
+			        System.out.println(infos[i] + ": " + String.join(", ", selectedJobs));
+			    }
+			}
+			
+			response.sendRedirect(request.getContextPath() + "/view/customer/customerList/customerList.jsp");
+			} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
