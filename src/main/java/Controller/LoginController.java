d9e0999554c5fa60586c38311f0bd442df669147
김민재 <lake041@korea.ac.kr>
2023-06-19 17:02:10 +0900

    Feat: 로그인 세션 구현 (#33)
    * Update: login.jsp 정리 및 URL 수정
    
    * Feat: 세션 로그인, 세션 남은시간 등 기본적인 기능 구현
    
    * Feat: 세션 로그인, 세션 남은시간 등 기본적인 기능 구현, 헤더 v1
    
    * Feat: 세션 로그인, 세션 남은시간 등 기본적인 기능 구현, 헤더 v2
    
    * Feat: 세션 로그인, 세션 남은시간 등 기본적인 기능 구현, 헤더 v3
    
    * Style: css 파일명 수정, 더미값 제거
    
    ---------
    
    Co-authored-by: ssangjun <105031887+ssangjun@users.noreply.github.com>

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -2,9 +2,7 @@ package Controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,16 +29,9 @@ public class LoginController extends HttpServlet {
 
 	protected void redirectWithErrorMessage(HttpServletRequest request, HttpServletResponse response,
 			String errorMessage, String employeeId) throws ServletException, IOException {
-		request.setAttribute("errorMessage", errorMessage);
-		if (employeeId != null)
-			request.setAttribute("employeeId", employeeId);
-		ServletContext app = this.getServletContext();
-		RequestDispatcher dispatcher = app.getRequestDispatcher("/view/login/login.jsp");
-		try {
-			dispatcher.forward(request, response);
-		} catch (ServletException e) {
-			System.out.println(e);
-		}
+		request.getSession().setAttribute("employeeId", employeeId);
+		request.getSession().setAttribute("errorMessage", errorMessage);
+		response.sendRedirect(request.getContextPath() + "/view/login/login.jsp");
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -64,9 +55,13 @@ public class LoginController extends HttpServlet {
 		EmployeeDTO employeeDTO = new EmployeeDTO();
 		employeeDTO.setEmployeeId(employeeId);
 		employeeDTO.setPassword(password);
-		boolean loginSuccess = LoginService.authenticateEmployee(employeeDTO);
+		EmployeeDTO storedEmployeeDTO = new EmployeeDTO();
+		storedEmployeeDTO = (EmployeeDTO) LoginService.authenticateEmployee(employeeDTO);
 
-		if (loginSuccess) {
+		if (storedEmployeeDTO != null) {
+			request.getSession().setAttribute("loginId", storedEmployeeDTO.getEmployeeId());
+			request.getSession().setAttribute("loginName", storedEmployeeDTO.getEmployeeName());
+			request.getSession().setAttribute("loginPosition", storedEmployeeDTO.getPosition());
 			response.sendRedirect(request.getContextPath() + "/view/main/main.jsp");
 		} else {
 			redirectWithErrorMessage(request, response, "아이디 또는 비밀번호를 잘못 입력했습니다.", String.valueOf(employeeId));
