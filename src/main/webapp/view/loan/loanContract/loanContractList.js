3f76145b72908f649f93ce36bde0744a60c17fbb
ykpark3 <ykpark183@gmail.com>
2023-06-21 09:46:27 +0900

    Feat: 테이블 클릭 시 팝업창 구현

--- src/main/webapp/view/loan/loanContract/loanContractList.js
+++ src/main/webapp/view/loan/loanContract/loanContractList.js
@@ -147,3 +147,21 @@ setYearSelect();
 setMonthSelect();
 setDaySelect(true);
 changeDate();
+
+// 팝업 창
+const searchTableRows = document.querySelectorAll('#loanSearchTable tr :not(th)');
+const popupBox = document.querySelector('.popupBox');
+const popupExitButton = document.querySelector('.popupExitButton');
+
+console.log("searchTableRows", searchTableRows);
+
+searchTableRows.forEach((item) => {
+	item.addEventListener('click', () => {
+		popupBox.classList.toggle('display');
+	});	
+});
+
+popupExitButton.addEventListener('click', () => {
+	popupBox.classList.toggle('display');
+});
+
