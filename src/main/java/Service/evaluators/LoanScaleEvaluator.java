507957ed2d2ac753c5207df8a1e39bf651c211f1
lake041 <lake041@korea.ac.kr>
2023-06-28 10:29:35 +0900

    Feat: 신용평가 시스템

--- src/main/java/Service/evaluators/LoanScaleEvaluator.java
+++ src/main/java/Service/evaluators/LoanScaleEvaluator.java
@@ -3,6 +3,8 @@ package Service.evaluators;
 import DTO.CreditScoringDTO;
 
 public class LoanScaleEvaluator extends EvaluatorParent implements EvaluatorInterface<Integer> {
+	private final int MAX_AMOUNT_STANDARD = 200000000;
+	private final int MAX_DURATION_STANDARD = 120;
 
 	@Override
 	public Integer getTarget(CreditScoringDTO creditScoringDTO) {
@@ -12,8 +14,38 @@ public class LoanScaleEvaluator extends EvaluatorParent implements EvaluatorInte
 	@Override
 	public int calculateScore(CreditScoringDTO creditScoringDTO) {
 		int score = 0;
+		int amountScore = 0;
+		int durationScore = 0;
 		long loanAmount = creditScoringDTO.getLoanAmount();
 		int loanDuration = creditScoringDTO.getLoanDuration();
+
+		if (loanAmount >= MAX_AMOUNT_STANDARD) {
+			amountScore = 0;
+		} else if (loanAmount >= MAX_AMOUNT_STANDARD / 2) {
+			amountScore = 10;
+		} else if (loanAmount >= MAX_AMOUNT_STANDARD / 3) {
+			amountScore = 20;
+		} else if (loanAmount >= MAX_AMOUNT_STANDARD / 5) {
+			amountScore = 30;
+		} else if (loanAmount >= MAX_AMOUNT_STANDARD / 10) {
+			amountScore = 50;
+		}
+
+		if (loanDuration >= MAX_DURATION_STANDARD) {
+			durationScore = 0;
+		} else if (loanDuration >= MAX_DURATION_STANDARD / 2) {
+			durationScore = 10;
+		} else if (loanDuration >= MAX_DURATION_STANDARD / 3) {
+			durationScore = 20;
+		} else if (loanDuration >= MAX_DURATION_STANDARD / 4) {
+			durationScore = 30;
+		} else if (loanDuration >= MAX_DURATION_STANDARD / 6) {
+			durationScore = 40;
+		} else if (loanDuration >= MAX_DURATION_STANDARD / 12) {
+			durationScore = 50;
+		}
+
+		score = amountScore + durationScore;
 
 		return (int) (score * getWeightForLoanScale());
 	}
