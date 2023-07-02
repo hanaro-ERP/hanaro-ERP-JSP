7ae214b3df5c4fbd2ec6cd1b8e1ae30c1a5205fb
YEONJEONG BAEK <84756243+baek0402@users.noreply.github.com>
2023-07-02 16:50:08 +0900

    Feature: 고객 가입시 보증인 유무선택 가능 (#86)
    * Feature: 보증인 검색 시 인풋값 유지
    
    * Update: 자잘한 오류 수정
    
    * Update: 자잘 오류 수정2
    
    * Feature: 고객 가입 보증인 유무
    
    * Feature: 자잘한 수정
    
    * Style: 스타일 소폭 수종
    
    ---------
    
    Co-authored-by: ssangjun <ssangjun4dev@gmail.com>

--- src/main/webapp/js/components/inputTable.js
+++ src/main/webapp/js/components/inputTable.js
@@ -2,6 +2,9 @@ function validateForm() {
     const inputs = document.querySelectorAll('input');
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].type !== 'hidden' && inputs[i].value === '') {
+            if (inputs[i].disabled) {
+            	return true;
+        	}
             alert('모든 항목을 입력해주세요.');
             return false;
         }
